87f8bf6190bbc30b54311384994916b73a30b91f
shankarj67 <shankarj67@gmail.com>
2019-12-26 21:04:56 +0530

    added django testdriven project

--- My_startup/oscar/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/skhk6342/60days/Github/Fullstack---Django/My_startup/oscar/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
